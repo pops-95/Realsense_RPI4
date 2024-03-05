15b2ebbe71b4a7ffd3bd176240797b55c570655e
pops-95 <layekfdi@gmail.com>
2024-03-05 16:37:11 +0530

    coordinate transformation

---
+++ coordinate.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pytransform3d.plot_utils import make_3d_axis
+import pytransform3d.transformations as pt
+
+R=np.eye(3)
+P=[-0.017,-0.157,-0.880]
+A2B=pt.transform_from(R,P)
+print("Transformation Matrix:")
+print(A2B)
+
+points=[-0.076,0.099,0.920,1]
+transformed=pt.transform(A2B,points)
+print("Transformed values =" +str(transformed))
